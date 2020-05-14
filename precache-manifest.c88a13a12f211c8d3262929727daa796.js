bc8afa94bace4b7cf602e8faee32c01f0fd63a77
BalboBigguns <micholyea@gmail.com>
2020-05-14 16:10:49 +0200

    Updates

---
+++ precache-manifest.c88a13a12f211c8d3262929727daa796.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c084e87b6e7dd95e589f5f99e40642cb",
+    "url": "/RandomQuoteGenerator/index.html"
+  },
+  {
+    "revision": "448ff0f67742760455f0",
+    "url": "/RandomQuoteGenerator/static/css/main.46b34096.chunk.css"
+  },
+  {
+    "revision": "1488bf1138a90044420d",
+    "url": "/RandomQuoteGenerator/static/js/2.f0e91fa3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RandomQuoteGenerator/static/js/2.f0e91fa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "448ff0f67742760455f0",
+    "url": "/RandomQuoteGenerator/static/js/main.2f19afd0.chunk.js"
+  },
+  {
+    "revision": "bd95c53e666ec3912ab9",
+    "url": "/RandomQuoteGenerator/static/js/runtime-main.730ab3ca.js"
+  }
+]);
